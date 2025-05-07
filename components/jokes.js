362af93e63c74f44d556ffa637f9c551781e4953
bluVshal <blu.vishal@gmail.com>
2025-05-07 23:55:05 +0400

    First commit

---
+++ components/jokes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Jokes = () => {
+  return (
+    <View>
+      <Text> Jokes </Text>
+    </View>
+  );
+};
+
+export default Jokes;
